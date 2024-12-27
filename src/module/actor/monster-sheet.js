e80c859fc6a72f43c5506f624a477a4dd22b0cbe
LeRatierBretonnien <sylvain@lahiette.com>
2024-12-27 21:21:31 +0100

    Fix default width for monster

--- src/module/actor/monster-sheet.js
+++ src/module/actor/monster-sheet.js
@@ -27,7 +27,7 @@ export class AcksActorSheetMonster extends AcksActorSheet {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["acks", "sheet", "monster", "actor"],
       template: "systems/acks/templates/actors/monster-sheet.html",
-      width: 480,
+      width: 500,
       height: 560,
       resizable: true,
       tabs: [
